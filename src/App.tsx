d2e7f96bd1053c3be84cde77c7229f7735fd05b7
Jayani Anuththara <jayanianuththara10@gmail.com>
2025-05-17 14:55:45 +0530

    Edit list of items

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   const list = ["Fruits", "Vegetables", "Groceries"];
   const [item, setItem] = useState([
     { id: 1, description: "Apple", category: "fruits", amount: 20 },
-    { id: 2, description: "Apple", category: "fruits", amount: 20 },
-    { id: 3, description: "Apple", category: "fruits", amount: 20 },
-    { id: 4, description: "Apple", category: "fruits", amount: 20 },
-    { id: 5, description: "Apple", category: "fruits", amount: 20 },
+    { id: 2, description: "Brinjal", category: "vegetables", amount: 10 },
+    { id: 3, description: "Dhal", category: "groceries", amount: 40 },
+    { id: 4, description: "Papaya", category: "fruits", amount: 40 },
+    { id: 5, description: "Wheat flour", category: "groceries", amount: 50 },
   ]);
 
   return (
